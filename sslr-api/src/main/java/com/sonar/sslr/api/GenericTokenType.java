b2ea1e1ca4445a6b7b0f7174816c832c83bc6c2e
Freddy Mallet <freddy.mallet@gmail.com>
2010-06-15 09:39:55 +0000

    Add a new Generic EOL (End Of Line) token type definition

--- sslr-api/src/main/java/com/sonar/sslr/api/GenericTokenType.java
+++ sslr-api/src/main/java/com/sonar/sslr/api/GenericTokenType.java
@@ -8,7 +8,7 @@ package com.sonar.sslr.api;
 import com.sonar.sslr.api.TokenType;
 
 public enum GenericTokenType implements TokenType {
-  COMMENT, IDENTIFIER, LITERAL, CONSTANT, EOF;
+  COMMENT, IDENTIFIER, LITERAL, CONSTANT, EOF, EOL;
 
   public String getName() {
     return name();
