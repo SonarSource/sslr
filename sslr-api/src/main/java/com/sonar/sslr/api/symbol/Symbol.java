29b850b0f87525669dabf629ba6f04c1e37c5110
Freddy Mallet <freddy.mallet@gmail.com>
2011-01-08 13:35:23 +0000

    fix SSLR-13 : Provide a basic mechanism to track use of Symbol(s)

---
+++ sslr-api/src/main/java/com/sonar/sslr/api/symbol/Symbol.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2010 SonarSource SA
+ * All rights reserved
+ * mailto:contact AT sonarsource DOT com
+ */
+package com.sonar.sslr.api.symbol;
+
+import com.sonar.sslr.api.AstNode;
+
+public abstract class Symbol {
+
+  private AstNode node;
+
+  public Symbol(AstNode node) {
+    this.node = node;
+  }
+
+  public int getStartLine() {
+    return node.getTokenLine();
+  }
+
+  public int getEndLine() {
+    return node.getLastToken().getLine();
+  }
+
+  public AstNode getAstNode() {
+    return node;
+  }
+}
