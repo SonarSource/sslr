35e40aedb8959f8fec37158f0c86db0638482fdf
Freddy Mallet <freddy.mallet@gmail.com>
2011-01-13 22:27:31 +0000

    SSLR-17 : Create a SSLR debug mode
    Deactivate the use of ThreadLocal to prevent any CPU overhead

--- sslr-impl/src/main/java/com/sonar/sslr/impl/loggers/ParserLogger.java
+++ sslr-impl/src/main/java/com/sonar/sslr/impl/loggers/ParserLogger.java
@@ -14,31 +14,31 @@ public final class ParserLogger {
   public static final String SSLR_MODE_PROPERTY = "sonar.sslr.mode";
   public static final String SSLR_DEBUG_MODE = "debug";
 
-  private static ThreadLocal<Logger> logger = new ThreadLocal<Logger>();
+  private static Logger logger;
 
   static {
     String sslrMode = System.getProperty(SSLR_MODE_PROPERTY);
     if (SSLR_DEBUG_MODE.equals(sslrMode)) {
       createParserLogger(new DebugLogger());
     } else {
-      logger.set(new EmptyLogger());
+      logger = new EmptyLogger();
     }
   }
 
   protected static void createParserLogger(Logger newLogger) {
-    logger.set(newLogger);
+    logger = newLogger;
   }
 
   public static void tryToMatch(Matcher matcher, ParsingState parsingState) {
-    logger.get().tryToMatch(matcher, parsingState);
+    logger.tryToMatch(matcher, parsingState);
   }
 
   public static void hasMatched(Matcher matcher, ParsingState parsingState, AstNode astNode) {
-    logger.get().hasMatched(matcher, parsingState, astNode);
+    logger.hasMatched(matcher, parsingState, astNode);
   }
 
   public static void memoizedAstUsed(Matcher matcher, ParsingState parsingState, AstNode astNode) {
-    logger.get().memoizedAstUsed(matcher, parsingState, astNode);
+    logger.memoizedAstUsed(matcher, parsingState, astNode);
 
   }
 
