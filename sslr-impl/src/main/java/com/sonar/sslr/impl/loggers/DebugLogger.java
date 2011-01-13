8e556f96880e3a98a95226f2d2179f824fce7fc7
Fabrice Bellingard <bellingard@gmail.com>
2011-01-13 17:49:16 +0000

    [SSLR-17] Create a SSLR debug mode http://jira.sonarsource.com/browse/SSLR-17
    Worked on the format of the output

--- sslr-impl/src/main/java/com/sonar/sslr/impl/loggers/DebugLogger.java
+++ sslr-impl/src/main/java/com/sonar/sslr/impl/loggers/DebugLogger.java
@@ -5,12 +5,7 @@
  */
 package com.sonar.sslr.impl.loggers;
 
-import org.slf4j.LoggerFactory;
-
-import ch.qos.logback.classic.Level;
-import ch.qos.logback.classic.PatternLayout;
-import ch.qos.logback.classic.spi.LoggingEvent;
-import ch.qos.logback.core.ConsoleAppender;
+import java.io.PrintStream;
 
 import com.sonar.sslr.api.AstNode;
 import com.sonar.sslr.impl.ParsingState;
@@ -18,33 +13,22 @@ import com.sonar.sslr.impl.matcher.Matcher;
 
 class DebugLogger implements Logger {
 
-  static final ch.qos.logback.classic.Logger consoleLogger = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger(DebugLogger.class);
+  private PrintStream logger;
 
-  static {
-    PatternLayout patternLayout = new PatternLayout();
-    patternLayout.setPattern("%-5level - %msg%n");
-    ConsoleAppender<LoggingEvent> appender = new ConsoleAppender<LoggingEvent>();
-    appender.setName("DebugLoggerAppender");
-    appender.setLayout(patternLayout);
-    consoleLogger.setLevel(Level.INFO);
-    consoleLogger.addAppender(appender);
-    // consoleLogger.setAdditive(false);
+  public DebugLogger() {
+    logger = System.out;
   }
 
   public void tryToMatch(Matcher matcher, ParsingState parsingState) {
-    consoleLogger.info("... Matcher \"" + matcher + "\" tries to match token #" + parsingState.lexerIndex + ": \""
-        + parsingState.readToken(parsingState.lexerIndex) + "\"");
+    logger.printf("--> %1$-6s %2$-30s %3$s%n", parsingState.lexerIndex, parsingState.readToken(parsingState.lexerIndex), matcher);
   }
 
   public void hasMatched(Matcher matcher, ParsingState parsingState, AstNode astNode) {
-    consoleLogger.info("==> Matcher \"" + matcher + "\" has matched \"" + parsingState.readToken(parsingState.lexerIndex)
-        + "\" and returns the following AST: \t" + astNode);
+    logger.printf("<== %1$-6s %2$-30s %3$s%n", parsingState.lexerIndex, parsingState.readToken(parsingState.lexerIndex), matcher, astNode);
   }
 
   public void memoizedAstUsed(Matcher matcher, ParsingState parsingState, AstNode astNode) {
-    consoleLogger.info("\t\t/!\\ Memoized cache used by matcher \"" + matcher + "\" for token "
-        + parsingState.readToken(parsingState.lexerIndex) + " (at position " + parsingState.lexerIndex
-        + ") and has returned the following AST: \t" + astNode);
+    logger.printf("< > %1$-6s %2$-30s %3$s%n", parsingState.lexerIndex, parsingState.readToken(parsingState.lexerIndex), matcher, astNode);
   }
 
 }
