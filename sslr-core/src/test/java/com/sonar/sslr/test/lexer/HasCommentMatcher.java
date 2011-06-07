7fce665d87c92784c1649a757f8379afd4c8be35
Freddy Mallet <freddy.mallet@gmail.com>
2011-06-07 20:32:29 +0000

    SSLR-66 sslr-core depends on sslr-lexer-testing-harness version 1.3, which does not exist in SNAPSHOT anymore.

---
+++ sslr-core/src/test/java/com/sonar/sslr/test/lexer/HasCommentMatcher.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2010 SonarSource SA
+ * All rights reserved
+ * mailto:contact AT sonarsource DOT com
+ */
+package com.sonar.sslr.test.lexer;
+
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+
+import com.sonar.sslr.api.LexerOutput;
+import com.sonar.sslr.api.Token;
+
+class HasCommentMatcher extends BaseMatcher<LexerOutput> {
+
+  private final String commentValue;
+  private final int commentLine;
+
+  HasCommentMatcher(String commentValue) {
+    this(commentValue, -1);
+  }
+
+  public HasCommentMatcher(String commentValue, int commentLine) {
+    this.commentValue = commentValue;
+    this.commentLine = commentLine;
+  }
+
+  public boolean matches(Object obj) {
+    if ( !(obj instanceof LexerOutput)) {
+      return false;
+    }
+    LexerOutput output = (LexerOutput) obj;
+    for (Token comment : output.getCommentTokens()) {
+      if (comment.getValue().equals(commentValue)) {
+        if (commentLine > -1 && comment.getLine() != commentLine) {
+          continue;
+        }
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public void describeTo(Description desc) {
+    desc.appendText("Comment('" + commentValue + "')");
+  }
+}
---
+++ sslr-core/src/test/java/com/sonar/sslr/test/parser/MatchMatcher.java
@@ -0,0 +1,53 @@
+package com.sonar.sslr.test.parser;
+
+/*
+ * Copyright (C) 2010 SonarSource SA
+ * All rights reserved
+ * mailto:contact AT sonarsource DOT com
+ */
+
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+
+import com.sonar.sslr.impl.Lexer;
+import com.sonar.sslr.impl.ParsingStackTrace;
+import com.sonar.sslr.impl.ParsingState;
+import com.sonar.sslr.impl.BacktrackingException;
+import com.sonar.sslr.impl.matcher.Matcher;
+
+class MatchMatcher extends BaseMatcher<Matcher> {
+
+  private final String sourceCode;
+  private final Lexer lexer;
+  private String parsingStackTrace;
+  private Matcher matcher;
+
+  public MatchMatcher(String sourceCode, Lexer lexer) {
+    this.sourceCode = sourceCode;
+    this.lexer = lexer;
+  }
+
+  public boolean matches(Object obj) {
+    if ( !(obj instanceof Matcher)) {
+      return false;
+    }
+    matcher = (Matcher) obj;
+    ParsingState parsingState = new ParsingState(lexer.lex(sourceCode).getTokens());
+    try {
+      matcher.match(parsingState);
+      return true;
+    } catch (BacktrackingException e) {
+      parsingStackTrace = ParsingStackTrace.generateFullStackTrace(parsingState);
+      return false;
+    }
+  }
+
+  public void describeTo(Description desc) {
+    if (parsingStackTrace != null) {
+      desc.appendText("The matcher '" + matcher + "' doesn't match the beginning of '" + sourceCode + "'.\n");
+      desc.appendText("Parsing stack trace : " + parsingStackTrace);
+    } else {
+      desc.appendText("The matcher '" + matcher + "' hasn't matched the overall expression '" + sourceCode + "'.\n");
+    }
+  }
+}
