9af55e395e1498a32b3a2cfb95bf58ab2ee182ad
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2011-08-10 09:28:15 +0000

    SSLR: Auto Completer now handles predicated matchers next() and not()

--- sslr-core/src/main/java/com/sonar/sslr/impl/events/AutoCompleter.java
+++ sslr-core/src/main/java/com/sonar/sslr/impl/events/AutoCompleter.java
@@ -10,44 +10,27 @@ import static com.sonar.sslr.impl.matcher.GrammarFunctions.Predicate.*;
 
 import java.util.*;
 
+import com.sonar.sslr.api.AstNode;
 import com.sonar.sslr.api.GenericTokenType;
 import com.sonar.sslr.api.Token;
 import com.sonar.sslr.impl.BacktrackingEvent;
 import com.sonar.sslr.impl.ParsingState;
-import com.sonar.sslr.impl.matcher.Matcher;
-import com.sonar.sslr.impl.matcher.TokenMatcher;
-import com.sonar.sslr.impl.matcher.TokenValueMatcher;
+import com.sonar.sslr.impl.matcher.*;
 
 public final class AutoCompleter extends ParsingEventListener {
 
   private static final int maxTokens = 5;
   
-  private final int maxLength;
-  private final Matcher matcher;
   private final List<List<Token>> partialMatches = new LinkedList<List<Token>>();
   private final List<List<Token>> fullMatches = new LinkedList<List<Token>>();
-  private List<List<Token>> prefixes = new LinkedList<List<Token>>();
   private final Set<TokenMatcher> followingTokenMatchers = new HashSet<TokenMatcher>();
   
-  @Override
-  public void exitWithoutMatchMatcher(Matcher matcher, ParsingState parsingState) {
-    if (parsingState.lexerIndex == parsingState.lexerSize && matcher instanceof TokenMatcher) {
-      followingTokenMatchers.add((TokenMatcher)matcher);
-    }
-  }
+  private int maxLength;
+  private List<List<Token>> prefixes = new LinkedList<List<Token>>();
+  private int predicateLevel;
   
-  public AutoCompleter(Matcher matcher) {
-    this(matcher, new ArrayList<Token>());
-  }
-  
-  public AutoCompleter(Matcher matcher, List<Token> tokens) {
-    this(matcher, tokens, maxTokens);
-  }
-  
-  public AutoCompleter(Matcher matcher, List<Token> tokens, int maxTokens) {
-    this.matcher = matcher;
-    this.maxLength = tokens.size() + maxTokens;
-    this.prefixes.add(tokens);
+  private boolean isPredicateMatcher(Matcher matcher) {
+    return (matcher instanceof NotMatcher || matcher instanceof NextMatcher);
   }
   
   private Token tokenMatcherToToken(TokenMatcher tokenMatcher) {
@@ -60,8 +43,40 @@ public final class AutoCompleter extends ParsingEventListener {
       throw new UnsupportedOperationException("tokenMatcherToToken() does not handle class " + tokenMatcher.getClass());
     }
   }
+  
+  @Override
+  public void enterMatcher(Matcher matcher, ParsingState parsingState) {
+    if (isPredicateMatcher(matcher)) predicateLevel++;
+  }
+  
+  @Override
+  public void exitWithMatchMatcher(Matcher matcher, ParsingState parsingState, AstNode astNode) {
+    if (isPredicateMatcher(matcher)) predicateLevel--;
+  }
 
-  public void autoComplete() {
+  @Override
+  public void exitWithoutMatchMatcher(Matcher matcher, ParsingState parsingState) {
+    if (predicateLevel == 0 && parsingState.lexerIndex == parsingState.lexerSize && matcher instanceof TokenMatcher) {
+      followingTokenMatchers.add((TokenMatcher)matcher);
+    }
+    
+    if (isPredicateMatcher(matcher)) predicateLevel--;
+  }
+  
+  public void autoComplete(Matcher matcher) {
+    autoComplete(matcher, new ArrayList<Token>());
+  }
+  
+  public void autoComplete(Matcher matcher, List<Token> tokens) {
+    autoComplete(matcher, tokens, maxTokens);
+  }
+
+  public void autoComplete(Matcher matcher, List<Token> tokens, int maxTokens) {
+    this.maxLength = tokens.size() + maxTokens;
+    this.prefixes.clear();
+    this.prefixes.add(tokens);
+    predicateLevel = 0;
+    
     while (!prefixes.isEmpty()) {
       List<List<Token>> newPrefixes = new LinkedList<List<Token>>();
       for (List<Token> prefix: prefixes) {
@@ -77,27 +92,22 @@ public final class AutoCompleter extends ParsingEventListener {
     	    /* The parse was actually successful, there is nothing to complete! */
     	    fullMatches.add(prefix);
     	  } catch (BacktrackingEvent re) {
-    	    if (parsingState.lexerIndex < parsingState.lexerSize) {
-    	      /* The given tokens are not a valid prefix for this matcher, failed to auto complete */
-    	      throw new IllegalStateException("parsingState.lexerIndex should never be smaller than parsingState.lexerSize!");
-    	    }
-    	    
     	    if (followingTokenMatchers.isEmpty()) {
-    	      /* The matcher did not match, but did also not consume any token matcher */
-    	      throw new IllegalStateException("Matcher did not match but did not consume any TokenMatcher!");
+    	      /* Overall the matcher did not match, but no progress was made */
+    	      throw new IllegalStateException("Matcher did not match and did not consume any additional TokenMatcher (this should never happen when using only Standard matchers).");
     	    }
     	    
-    	    /* completeCandidates contains the candidates for auto completion! */
-    	    for (TokenMatcher followingTokenMatcher: followingTokenMatchers) {
-    	      LinkedList<Token> newPrefix = new LinkedList<Token>();
-    	      newPrefix.addAll(prefix);
-    	      newPrefix.add(tokenMatcherToToken(followingTokenMatcher));
-    	      
-    	      if (newPrefix.size() >= maxLength) {
-    	        partialMatches.add(newPrefix);
-    	      } else {
-    	        newPrefixes.add(newPrefix);
-    	      }
+    	    if (prefix.size() < maxLength) {
+      	    /* completeCandidates contains the candidates for auto completion! */
+      	    for (TokenMatcher followingTokenMatcher: followingTokenMatchers) {
+      	      LinkedList<Token> newPrefix = new LinkedList<Token>();
+      	      newPrefix.addAll(prefix);
+      	      newPrefix.add(tokenMatcherToToken(followingTokenMatcher));
+  
+      	      newPrefixes.add(newPrefix);
+      	    }
+    	    } else {
+    	      partialMatches.add(prefix);
     	    }
     	  }
       }
@@ -107,11 +117,14 @@ public final class AutoCompleter extends ParsingEventListener {
 	
 	public static void main(String[] args) {
 	  
-	  AutoCompleter auto = new AutoCompleter(
-	      and("hello", or("Foo", "Bar"))
-	  );
+	  AutoCompleter auto = new AutoCompleter();
 	  
-	  auto.autoComplete();
+	  auto.autoComplete(
+	       or(
+	           "fail",
+	           and("fail", "ure")
+	         )    
+	  );
 
     System.out.println("Full matches:");
     for (List<Token> tokens: auto.fullMatches) {
