73adb93fa9c7040eaf8e4dbbb8c1a6abb204f60c
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2012-01-25 11:48:57 +0000

    SSLR-138 Provide a LeftAssociativeIterator on AstNode which return a LeftAssociativeOperator

---
+++ sslr-core/src/main/java/com/sonar/sslr/api/LeftAssociative.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (C) 2010 SonarSource SA
+ * All rights reserved
+ * mailto:contact AT sonarsource DOT com
+ */
+package com.sonar.sslr.api;
+
+import org.apache.commons.lang.NotImplementedException;
+
+/**
+ * Utility class which helps to deal with left associative rules, which typically have the form:
+ * 
+ * <pre>
+ * higherPrecendenceExpressionRule.is(
+ *     lowerPrecedenceExpressionRule,
+ *     o2n(highPrecedenceOperator, lowerPrecedenceExpressionRule)
+ *     ).skipIfOneChild();
+ * </pre>
+ * 
+ * Let's take as an example a simple calculator with the binary subtraction as only operator:
+ * 
+ * <pre>
+ * primaryExpression.is(INTEGER_LITERAL);
+ * 
+ * substractionExpression.is(
+ *     primaryExpression,
+ *     o2n(&quot;-&quot;, primaryExpression)
+ *     ).skipIfOneChild();
+ * </pre>
+ * 
+ * The interpretation of the subtraction is made easy by this class, and is done as follows:
+ * 
+ * <pre>
+ * public int evaluate(AstNode node) {
+ *   if (node.getType() == primaryExpression) {
+ *     return evaluatePrimaryExpression(node);
+ *   } else if (node.getType() == substractionExpression) {
+ *     return evaluateSubtractionExpression(node);
+ *   } else {
+ *     throw new IllegalArgumentException(&quot;Unsupported node type &quot; + node.getType());
+ *   }
+ * }
+ * 
+ * private int evaluatePrimaryExpression(AstNode node) {
+ *   return Integer.parseInt(node.getTokenOriginalValue());
+ * }
+ * 
+ * private int evaluateSubtractionExpression(AstNode node) {
+ *   int accumulator = evaluate(node.getChild(0));
+ * 
+ *   for (LeftAssociative.OperatorAndOperand operatorAndOperand : new LeftAssociative.Iterator(node)) {
+ *     // If several binary operators have the same precedence, use operatorAndOperand.getOperator()
+ *     accumulator -= evaluate(operatorAndOperand.getOperand());
+ *   }
+ * 
+ *   return accumulator;
+ * }
+ * </pre>
+ */
+public final class LeftAssociative {
+
+  private LeftAssociative() {
+  }
+
+  public static class OperatorAndOperand {
+
+    private final AstNodeType operator;
+    private final AstNode operand;
+
+    public OperatorAndOperand(AstNodeType operator, AstNode operand) {
+      this.operator = operator;
+      this.operand = operand;
+    }
+
+    public AstNodeType getOperator() {
+      return operator;
+    }
+
+    public AstNode getOperand() {
+      return operand;
+    }
+
+  }
+
+  public static class Iterator implements java.util.Iterator<OperatorAndOperand>, java.lang.Iterable<OperatorAndOperand> {
+
+    private final AstNode leftAssociativeNode;
+    private int index;
+
+    public Iterator(AstNode leftAssociativeNode) {
+      this.leftAssociativeNode = leftAssociativeNode;
+      index = 1;
+    }
+
+    public boolean hasNext() {
+      return index + 1 < leftAssociativeNode.getNumberOfChildren();
+    }
+
+    public OperatorAndOperand next() {
+      OperatorAndOperand operatorAndOperand = new OperatorAndOperand(leftAssociativeNode.getChild(index).getType(),
+          leftAssociativeNode.getChild(index + 1));
+
+      index += 2;
+
+      return operatorAndOperand;
+    }
+
+    public void remove() {
+      throw new NotImplementedException();
+    }
+
+    public java.util.Iterator<OperatorAndOperand> iterator() {
+      return this;
+    }
+
+  }
+
+}
---
+++ sslr-tests/src/test/java/com/sonar/sslr/api/LeftAssociativeTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2010 SonarSource SA
+ * All rights reserved
+ * mailto:contact AT sonarsource DOT com
+ */
+package com.sonar.sslr.api;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+
+public class LeftAssociativeTest {
+
+  @Test
+  public void testOperatorAndOperand() {
+    AstNodeType operator = mock(AstNodeType.class);
+    AstNode operand = mock(AstNode.class);
+
+    LeftAssociative.OperatorAndOperand operatorAndOperand = new LeftAssociative.OperatorAndOperand(operator, operand);
+
+    assertThat(operatorAndOperand.getOperator(), is(operator));
+    assertThat(operatorAndOperand.getOperand(), is(operand));
+  }
+
+  @Test
+  public void testIterator() {
+    AstNode operand1 = mock(AstNode.class);
+
+    AstNode operator1 = mock(AstNode.class);
+    AstNodeType operator1Type = mock(AstNodeType.class);
+    when(operator1.getType()).thenReturn(operator1Type);
+
+    AstNode operand2 = mock(AstNode.class);
+
+    AstNode operator2 = mock(AstNode.class);
+    AstNodeType operator2Type = mock(AstNodeType.class);
+    when(operator2.getType()).thenReturn(operator2Type);
+
+    AstNode operand3 = mock(AstNode.class);
+
+    AstNode node = mock(AstNode.class);
+    when(node.getChild(0)).thenReturn(operand1);
+    when(node.getChild(1)).thenReturn(operator1);
+    when(node.getChild(2)).thenReturn(operand2);
+    when(node.getChild(3)).thenReturn(operator2);
+    when(node.getChild(4)).thenReturn(operand3);
+    when(node.getNumberOfChildren()).thenReturn(5);
+
+    LeftAssociative.Iterator iterator = new LeftAssociative.Iterator(node);
+
+    assertThat(iterator.hasNext(), is(true));
+    LeftAssociative.OperatorAndOperand operandAndOperator = iterator.next();
+    assertThat(operandAndOperator.getOperator(), is(operator1Type));
+    assertThat(operandAndOperator.getOperand(), is(operand2));
+    assertThat(iterator.hasNext(), is(true));
+    operandAndOperator = iterator.next();
+    assertThat(operandAndOperator.getOperator(), is(operator2Type));
+    assertThat(operandAndOperator.getOperand(), is(operand3));
+    assertThat(iterator.hasNext(), is(false));
+  }
+
+}
