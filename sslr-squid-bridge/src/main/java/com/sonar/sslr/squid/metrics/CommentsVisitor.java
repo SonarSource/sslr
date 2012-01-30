649b397a96b6c0ee45241280f84165f46fc8e994
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2012-01-30 09:16:30 +0000

    SSLR Update the comments bridge to use the Trivia API instead of old getComments() one, now that C injects properly all the trivia

--- sslr-squid-bridge/src/main/java/com/sonar/sslr/squid/metrics/CommentsVisitor.java
+++ sslr-squid-bridge/src/main/java/com/sonar/sslr/squid/metrics/CommentsVisitor.java
@@ -11,10 +11,7 @@ import java.util.Set;
 import org.sonar.squid.api.SourceFile;
 import org.sonar.squid.measures.MetricDef;
 
-import com.sonar.sslr.api.AstAndTokenVisitor;
-import com.sonar.sslr.api.AstNode;
-import com.sonar.sslr.api.Grammar;
-import com.sonar.sslr.api.Token;
+import com.sonar.sslr.api.*;
 import com.sonar.sslr.squid.SquidAstVisitor;
 
 /**
@@ -25,7 +22,7 @@ public final class CommentsVisitor<GRAMMAR extends Grammar> extends SquidAstVisi
   private Set<Integer> noSonar;
   private Set<Integer> comments;
   private Set<Integer> blankComments;
-  private Token firstToken;
+  private boolean seenFirstToken;
 
   private final boolean enableNoSonar;
   private final MetricDef commentMetric;
@@ -72,13 +69,36 @@ public final class CommentsVisitor<GRAMMAR extends Grammar> extends SquidAstVisi
     noSonar = new HashSet<Integer>();
     comments = new HashSet<Integer>();
     blankComments = new HashSet<Integer>();
-    firstToken = null;
+    seenFirstToken = false;
   }
 
   public void visitToken(Token token) {
-    if (ignoreHeaderComments && firstToken == null) {
-      firstToken = token;
+    if ( !ignoreHeaderComments || ignoreHeaderComments && seenFirstToken) {
+      for (Trivia trivia : token.getTrivia()) {
+        if (trivia.isComment()) {
+          String[] commentLines = getContext().getCommentAnalyser().getContents(trivia.getToken().getOriginalValue())
+              .split("(\r)?\n|\r", -1);
+          int line = trivia.getToken().getLine();
+
+          for (String commentLine : commentLines) {
+            if (enableNoSonar && commentLine.contains("NOSONAR")) {
+              /* NOSONAR */
+              addNoSonar(line);
+            } else if (blankCommentMetric != null && getContext().getCommentAnalyser().isBlank(commentLine)) {
+              /* Blank lines */
+              addBlankCommentLine(line);
+            } else if (commentMetric != null) {
+              /* Comment lines */
+              addCommentLine(line);
+            }
+
+            line++;
+          }
+        }
+      }
     }
+
+    seenFirstToken = true;
   }
 
   /**
@@ -86,32 +106,6 @@ public final class CommentsVisitor<GRAMMAR extends Grammar> extends SquidAstVisi
    */
   @Override
   public void leaveFile(AstNode astNode) {
-    if (getContext().getComments() != null) {
-      for (Token commentToken : getContext().getComments()) {
-        if (ignoreHeaderComments && firstToken != null && isTokenBefore(commentToken, firstToken)) {
-          continue;
-        }
-
-        String[] commentLines = getContext().getCommentAnalyser().getContents(commentToken.getOriginalValue()).split("(\r)?\n|\r", -1);
-        int line = commentToken.getLine();
-
-        for (String commentLine : commentLines) {
-          if (enableNoSonar && commentLine.contains("NOSONAR")) {
-            /* NOSONAR */
-            addNoSonar(line);
-          } else if (blankCommentMetric != null && getContext().getCommentAnalyser().isBlank(commentLine)) {
-            /* Blank lines */
-            addBlankCommentLine(line);
-          } else if (commentMetric != null) {
-            /* Comment lines */
-            addCommentLine(line);
-          }
-
-          line++;
-        }
-      }
-    }
-
     if (enableNoSonar) {
       ((SourceFile) getContext().peekSourceCode()).addNoSonarTagLines(noSonar);
     }
@@ -121,10 +115,6 @@ public final class CommentsVisitor<GRAMMAR extends Grammar> extends SquidAstVisi
     if (blankCommentMetric != null) {
       getContext().peekSourceCode().add(blankCommentMetric, blankComments.size());
     }
-  }
-
-  private boolean isTokenBefore(Token a, Token b) {
-    return a.getLine() < b.getLine() || a.getLine() == b.getLine() && a.getColumn() < b.getColumn();
   }
 
   public static <GRAMMAR extends Grammar> CommentsVisitorBuilder<GRAMMAR> builder() {
