a4f184d73f11f0451c63210c3bc974396d3cb39e
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2012-02-01 13:50:35 +0000

    SSLR Add a Token.onSameLine(Token other) method

--- sslr-core/src/main/java/com/sonar/sslr/api/Token.java
+++ sslr-core/src/main/java/com/sonar/sslr/api/Token.java
@@ -159,6 +159,10 @@ public class Token {
     }
   }
 
+  public boolean onSameLineThan(Token other) {
+    return other == null ? false : getLine() == other.getLine();
+  }
+
   @Override
   public int hashCode() {
     final int prime = 31;
