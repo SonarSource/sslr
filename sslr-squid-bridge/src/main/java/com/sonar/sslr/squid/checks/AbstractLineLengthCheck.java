c3661d859d8873f83617d2a1904b73496c9d6566
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2012-02-21 14:27:18 +0000

    SSLR Fix the generic line length check to skip generated code

--- sslr-squid-bridge/src/main/java/com/sonar/sslr/squid/checks/AbstractLineLengthCheck.java
+++ sslr-squid-bridge/src/main/java/com/sonar/sslr/squid/checks/AbstractLineLengthCheck.java
@@ -5,12 +5,11 @@
  */
 package com.sonar.sslr.squid.checks;
 
-import org.sonar.api.utils.SonarException;
-
 import com.sonar.sslr.api.AstAndTokenVisitor;
 import com.sonar.sslr.api.AstNode;
 import com.sonar.sslr.api.Grammar;
 import com.sonar.sslr.api.Token;
+import org.sonar.api.utils.SonarException;
 
 public abstract class AbstractLineLengthCheck<GRAMMAR extends Grammar> extends SquidCheck<GRAMMAR> implements AstAndTokenVisitor {
 
@@ -23,8 +22,8 @@ public abstract class AbstractLineLengthCheck<GRAMMAR extends Grammar> extends S
   public void init() {
     if (getMaximumLineLength() <= 0) {
       throw new SonarException("[AbstractLineLengthCheck] The maximal line length must be set to a value greater than 0 ("
-          + getMaximumLineLength()
-          + " given).");
+        + getMaximumLineLength()
+        + " given).");
     }
   }
 
@@ -34,7 +33,7 @@ public abstract class AbstractLineLengthCheck<GRAMMAR extends Grammar> extends S
   }
 
   public void visitToken(Token token) {
-    if (lastIncorrectLine != token.getLine() && token.getColumn() + token.getValue().length() > getMaximumLineLength()) {
+    if (!token.isGeneratedCode() && lastIncorrectLine != token.getLine() && token.getColumn() + token.getValue().length() > getMaximumLineLength()) {
       lastIncorrectLine = token.getLine();
       getContext().createLineViolation(this, "The line length is greater than {0,number,integer} authorized.", token.getLine(), getMaximumLineLength());
     }
