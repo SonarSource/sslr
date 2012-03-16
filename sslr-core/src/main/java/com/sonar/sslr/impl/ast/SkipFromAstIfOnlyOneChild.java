eced462aa5c37e8196f7a98b98ace681b10ef366
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2012-03-16 09:56:15 +0000

    SSLR Remove a useless method

--- sslr-core/src/main/java/com/sonar/sslr/impl/ast/SkipFromAstIfOnlyOneChild.java
+++ sslr-core/src/main/java/com/sonar/sslr/impl/ast/SkipFromAstIfOnlyOneChild.java
@@ -7,16 +7,12 @@ package com.sonar.sslr.impl.ast;
 
 import com.sonar.sslr.api.AstNode;
 import com.sonar.sslr.api.AstNodeSkippingPolicy;
-import com.sonar.sslr.api.AstNodeType;
 
 public class SkipFromAstIfOnlyOneChild implements AstNodeSkippingPolicy {
 
+  @Override
   public boolean hasToBeSkippedFromAst(AstNode node) {
     return node.getNumberOfChildren() == 1;
-  }
-
-  public static AstNodeType oneChild() {
-    return new SkipFromAstIfOnlyOneChild();
   }
 
 }
