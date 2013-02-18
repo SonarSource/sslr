ea999ea2f400aec417ea6b06b28f792ae988e57a
Evgeny Mandrikov <mandrikov@gmail.com>
2013-02-18 12:11:00 +0100

    Add example of grammar for the classic non-context-free language
    Just because it is cool to have such example.

---
+++ sslr-examples/src/main/java/org/sonar/sslr/examples/abc/AbcGrammar.java
@@ -0,0 +1,43 @@
+/*
+ * SonarSource Language Recognizer
+ * Copyright (C) 2010 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.sslr.examples.abc;
+
+import org.sonar.sslr.grammar.GrammarRuleKey;
+import org.sonar.sslr.grammar.LexerlessGrammarBuilder;
+import org.sonar.sslr.parser.LexerlessGrammar;
+
+/**
+ * Grammar for the classic non-context-free language { a^n b^n c^n : n >= 1 }.
+ */
+public enum AbcGrammar implements GrammarRuleKey {
+
+  S, A, B;
+
+  public static LexerlessGrammar createGrammar() {
+    LexerlessGrammarBuilder b = LexerlessGrammarBuilder.create();
+
+    b.rule(S).is(b.next(A, "c"), b.oneOrMore("a"), B, b.nextNot("a", "b", "c"));
+    b.rule(A).is("a", b.optional(A), "b");
+    b.rule(B).is("b", b.optional(B), "c");
+
+    return b.build();
+  }
+
+}
