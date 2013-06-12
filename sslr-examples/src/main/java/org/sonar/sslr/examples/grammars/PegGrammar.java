abddcbaf116e92462f677cd42524cfd9f6938cb2
Evgeny Mandrikov <mandrikov@gmail.com>
2013-06-13 00:53:48 +0200

    Add grammar for PEG

---
+++ sslr-examples/src/main/java/org/sonar/sslr/examples/grammars/PegGrammar.java
@@ -0,0 +1,98 @@
+/*
+ * SonarSource Language Recognizer
+ * Copyright (C) 2010 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.sslr.examples.grammars;
+
+import com.sonar.sslr.api.Grammar;
+import org.sonar.sslr.grammar.GrammarRuleKey;
+import org.sonar.sslr.grammar.LexerlessGrammarBuilder;
+
+/**
+ * PEG grammar.
+ */
+public enum PegGrammar implements GrammarRuleKey {
+
+  GRAMMAR,
+  RULE,
+  RULE_KEY,
+  STRING,
+  ATOM,
+
+  FIRST_OF_EXPRESSION,
+  SEQUENCE_EXPRESSION,
+  ZERO_OR_MORE_EXPRESSION,
+  ONE_OR_MORE_EXPRESSION,
+  OPTIONAL_EXPRESSION,
+  NEXT_NOT_EXPRESSION,
+  NEXT_EXPRESSION,
+
+  ASSIGN,
+  SEMICOLON,
+  OR,
+  NOT,
+  AND,
+  QUESTION,
+  PLUS,
+  STAR,
+  OPEN,
+  CLOSE,
+  WHITESPACE;
+
+  public static Grammar create() {
+    LexerlessGrammarBuilder b = LexerlessGrammarBuilder.create();
+
+    b.rule(GRAMMAR).is(WHITESPACE, b.zeroOrMore(RULE), b.endOfInput());
+    b.rule(RULE).is(RULE_KEY, ASSIGN, FIRST_OF_EXPRESSION, SEMICOLON);
+    b.rule(FIRST_OF_EXPRESSION).is(SEQUENCE_EXPRESSION, b.zeroOrMore(OR, SEQUENCE_EXPRESSION));
+    b.rule(SEQUENCE_EXPRESSION).is(b.oneOrMore(b.firstOf(
+      ZERO_OR_MORE_EXPRESSION,
+      ONE_OR_MORE_EXPRESSION,
+      OPTIONAL_EXPRESSION,
+      NEXT_NOT_EXPRESSION,
+      NEXT_EXPRESSION,
+      ATOM)));
+    b.rule(ZERO_OR_MORE_EXPRESSION).is(ATOM, STAR);
+    b.rule(ONE_OR_MORE_EXPRESSION).is(ATOM, PLUS);
+    b.rule(OPTIONAL_EXPRESSION).is(ATOM, QUESTION);
+    b.rule(NEXT_NOT_EXPRESSION).is(NOT, ATOM);
+    b.rule(NEXT_EXPRESSION).is(AND, ATOM);
+    b.rule(ATOM).is(b.firstOf(
+      RULE_KEY,
+      STRING,
+      b.sequence(OPEN, FIRST_OF_EXPRESSION, CLOSE)));
+
+    b.rule(RULE_KEY).is(b.regexp("\\p{javaJavaIdentifierStart}\\p{javaJavaIdentifierPart}*+"), WHITESPACE);
+    b.rule(STRING).is(b.regexp("\"[^\"]*+\""), WHITESPACE);
+
+    b.rule(ASSIGN).is("=", WHITESPACE);
+    b.rule(SEMICOLON).is(";", WHITESPACE);
+    b.rule(OR).is("|", WHITESPACE);
+    b.rule(NOT).is("!", WHITESPACE);
+    b.rule(AND).is("&", WHITESPACE);
+    b.rule(QUESTION).is("?", WHITESPACE);
+    b.rule(PLUS).is("+", WHITESPACE);
+    b.rule(STAR).is("*", WHITESPACE);
+    b.rule(OPEN).is("(", WHITESPACE);
+    b.rule(CLOSE).is(")", WHITESPACE);
+    b.rule(WHITESPACE).is(b.regexp("\\s*+"));
+
+    return b.build();
+  }
+
+}
---
+++ sslr-examples/src/test/java/org/sonar/sslr/examples/grammars/PegGrammarTest.java
@@ -0,0 +1,65 @@
+/*
+ * SonarSource Language Recognizer
+ * Copyright (C) 2010 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.sslr.examples.grammars;
+
+import com.sonar.sslr.api.Grammar;
+import org.junit.Test;
+
+import static org.sonar.sslr.tests.Assertions.assertThat;
+
+public class PegGrammarTest {
+
+  private Grammar g = PegGrammar.create();
+
+  @Test
+  public void test() {
+    assertThat(g.rule(PegGrammar.WHITESPACE))
+      .matches("\n \r \t");
+    assertThat(g.rule(PegGrammar.STRING))
+      .matches("\"string\"");
+    assertThat(g.rule(PegGrammar.RULE_KEY))
+      .matches("rule_key");
+    assertThat(g.rule(PegGrammar.ATOM))
+      .matches("\"string\"")
+      .matches("rule_key")
+      .matches("( e )");
+    assertThat(g.rule(PegGrammar.ZERO_OR_MORE_EXPRESSION))
+      .matches("e *");
+    assertThat(g.rule(PegGrammar.ONE_OR_MORE_EXPRESSION))
+      .matches("e +");
+    assertThat(g.rule(PegGrammar.OPTIONAL_EXPRESSION))
+      .matches("e ?");
+    assertThat(g.rule(PegGrammar.NEXT_NOT_EXPRESSION))
+      .matches("! e");
+    assertThat(g.rule(PegGrammar.NEXT_EXPRESSION))
+      .matches("& e");
+    assertThat(g.rule(PegGrammar.SEQUENCE_EXPRESSION))
+      .matches("e e");
+    assertThat(g.rule(PegGrammar.FIRST_OF_EXPRESSION))
+      .matches("e")
+      .matches("e | e");
+    assertThat(g.rule(PegGrammar.RULE))
+      .matches("rule = e ;")
+      .matches("rule = e | e ;");
+    assertThat(g.rule(PegGrammar.GRAMMAR))
+      .matches("rule = e ; rule = e ;");
+  }
+
+}
