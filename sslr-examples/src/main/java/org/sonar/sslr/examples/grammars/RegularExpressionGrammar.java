a078cfdc46c08214495b9027f2b5d967a4a533b8
Evgeny Mandrikov <mandrikov@gmail.com>
2013-06-14 00:10:56 +0200

    Add grammar for Regular Expression

---
+++ sslr-examples/src/main/java/org/sonar/sslr/examples/grammars/RegularExpressionGrammar.java
@@ -0,0 +1,103 @@
+/*
+ * SonarSource Language Recognizer
+ * Copyright (C) 2010 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.sslr.examples.grammars;
+
+import com.sonar.sslr.api.Grammar;
+import org.sonar.sslr.grammar.GrammarRuleKey;
+import org.sonar.sslr.grammar.LexerlessGrammarBuilder;
+
+/**
+ * Regular Expression Grammar.
+ */
+public enum RegularExpressionGrammar implements GrammarRuleKey {
+
+  EXPRESSION,
+
+  ALTERNATION,
+  ALTERNATIVE,
+
+  TERM,
+  CAPTURING_GROUP,
+  NON_CAPTURING_GROUP,
+  POSITIVE_LOOKAHEAD,
+  NEGATIVE_LOOKAHEAD,
+  QUANTIFIER,
+  NUMBER,
+
+  ATOM,
+  BACK_REFERENCE,
+
+  CHARACTER_CLASS,
+  CLASS_ATOM;
+
+  public static Grammar create() {
+    LexerlessGrammarBuilder b = LexerlessGrammarBuilder.create();
+
+    b.rule(EXPRESSION).is(ALTERNATION);
+
+    b.rule(ALTERNATION).is(ALTERNATIVE, b.zeroOrMore("|", ALTERNATIVE));
+    b.rule(ALTERNATIVE).is(b.zeroOrMore(TERM));
+
+    b.rule(TERM).is(
+      b.firstOf(
+        ATOM,
+        POSITIVE_LOOKAHEAD,
+        NEGATIVE_LOOKAHEAD,
+        NON_CAPTURING_GROUP,
+        CAPTURING_GROUP
+      ),
+      b.optional(QUANTIFIER)
+    );
+    b.rule(POSITIVE_LOOKAHEAD).is("(?=", ALTERNATION, ")");
+    b.rule(NEGATIVE_LOOKAHEAD).is("(?!", ALTERNATION, ")");
+    b.rule(CAPTURING_GROUP).is("(", ALTERNATION, ")");
+    b.rule(NON_CAPTURING_GROUP).is("(?:", ALTERNATION, ")");
+    b.rule(QUANTIFIER).is(
+      b.firstOf(
+        "?",
+        "*",
+        "+",
+        b.sequence("{", NUMBER, "}"),
+        b.sequence("{", NUMBER, ",", "}"),
+        b.sequence("{", NUMBER, ",", NUMBER, "}")
+      ),
+      b.optional(b.firstOf("+", "?"))
+    );
+    b.rule(NUMBER).is(b.regexp("[0-9]++"));
+
+    b.rule(ATOM).is(b.firstOf(
+      ".",
+      BACK_REFERENCE,
+      b.sequence(b.nextNot(b.firstOf('[', '*', '+', '?', '(', ')', '|')), b.regexp(".")),
+      CHARACTER_CLASS
+    ));
+    b.rule(BACK_REFERENCE).is("\\", b.regexp("[1-9]"));
+
+    b.rule(CHARACTER_CLASS).is('[', b.optional("^"), b.oneOrMore(CLASS_ATOM), ']');
+    b.rule(CLASS_ATOM).is(b.firstOf(
+      b.sequence('\\', b.regexp(".")),
+      b.sequence(b.optional("&&"), CHARACTER_CLASS),
+      b.sequence(b.nextNot(b.firstOf('[', ']', "&&")), b.regexp("."))
+    ));
+
+    return b.build();
+  }
+
+}
---
+++ sslr-examples/src/test/java/org/sonar/sslr/examples/grammars/RegularExpressionGrammarTest.java
@@ -0,0 +1,155 @@
+/*
+ * SonarSource Language Recognizer
+ * Copyright (C) 2010 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.sslr.examples.grammars;
+
+import com.sonar.sslr.api.Grammar;
+import org.junit.Test;
+
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+import static org.sonar.sslr.tests.Assertions.assertThat;
+
+public class RegularExpressionGrammarTest {
+
+  private Grammar g = RegularExpressionGrammar.create();
+
+  @Test
+  public void character_class() {
+    assertThat(g.rule(RegularExpressionGrammar.CHARACTER_CLASS))
+      /* simple class */
+      .matches(correct("[abc]"))
+      .matches(correct("[&]"))
+      .matches(correct("[\\^]"))
+      .matches(correct("[\\]]"))
+      .matches(correct("[\\[]"))
+      /* negation */
+      .matches(correct("[^abc]"))
+      .matches(correct("[^^]"))
+      /* range */
+      .matches(correct("[a-z]"))
+      .matches(correct("[a-zA-Z]"))
+      /* union */
+      .matches(correct("[a-d[m-p]]"))
+      .matches(correct("[[a-z]]"))
+      /* intersection */
+      .matches(correct("[a-z&&[def]]"))
+      .matches(correct("[&&[a-z]]"))
+      /* subtraction */
+      .matches(correct("[a-z&&[^bc]]"))
+      .matches(correct("[a-z&&[^m-p]]"))
+      /* incorrect */
+      .notMatches(incorrect("[]"))
+      .notMatches(incorrect("[\\]"))
+      .notMatches(incorrect("[[]"))
+      .notMatches(incorrect("[a[]"))
+      .notMatches(incorrect("[^]"))
+      .notMatches(incorrect("[&&]"));
+  }
+
+  @Test
+  public void atom() {
+    assertThat(g.rule(RegularExpressionGrammar.ATOM))
+      /* character */
+      .matches(correct("a"))
+      .matches(correct("]"))
+      /* character class */
+      .matches(correct("[a-z]"))
+      /* any character */
+      .matches(correct("."))
+      /* back reference */
+      .matches(correct("\\1"))
+      /* incorrect */
+      .notMatches(incorrect("\\0"))
+      .notMatches(incorrect("["))
+      .notMatches(incorrect("("))
+      .notMatches(incorrect(")"))
+      .notMatches(incorrect("*"))
+      .notMatches(incorrect("+"))
+      .notMatches(incorrect("?"))
+      .notMatches("|");
+  }
+
+  @Test
+  public void term() {
+    assertThat(g.rule(RegularExpressionGrammar.TERM))
+      /* atom */
+      .matches(correct("a"))
+      /* atom with quantifier */
+      .matches(correct("a++"))
+      /* capturing group */
+      .matches(correct("()"))
+      /* non-capturing group */
+      .matches(correct("(?:)"))
+      /* zero-width positive lookahead */
+      .matches(correct("(?=)"))
+      /* zero-width negative lookahead */
+      .matches(correct("(?!)"));
+  }
+
+  @Test
+  public void alternation() {
+    assertThat(g.rule(RegularExpressionGrammar.ALTERNATION))
+      .matches("")
+      .matches(correct("a|b"))
+      .matches(correct("a|b|c"))
+      .matches(correct("|"));
+  }
+
+  @Test
+  public void quantifier() {
+    assertThat(g.rule(RegularExpressionGrammar.QUANTIFIER))
+      /* greedy */
+      .matches("*")
+      .matches("+")
+      .matches("?")
+      .matches("{1}")
+      .matches("{1,}")
+      .matches("{1,2}")
+      /* reluctant */
+      .matches("*?")
+      .matches("+?")
+      .matches("??")
+      .matches("{1}?")
+      .matches("{1,}?")
+      .matches("{1,2}?")
+      /* possessive */
+      .matches("*+")
+      .matches("++")
+      .matches("?+")
+      .matches("{1}+")
+      .matches("{1,}+")
+      .matches("{1,2}+");
+  }
+
+  private static String incorrect(String pattern) {
+    try {
+      Pattern.compile(pattern);
+      throw new AssertionError(pattern);
+    } catch (PatternSyntaxException e) {
+      return pattern;
+    }
+  }
+
+  private static String correct(String pattern) {
+    return Pattern.compile(pattern).pattern();
+  }
+
+}
