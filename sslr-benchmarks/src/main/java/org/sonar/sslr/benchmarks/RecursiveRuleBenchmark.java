aef74741f525dd925a4e32c07bc34aef550e444c
Evgeny Mandrikov <mandrikov@gmail.com>
2013-09-10 21:28:40 +0200

    Add RecursiveRuleBenchmark

---
+++ sslr-benchmarks/src/main/java/org/sonar/sslr/benchmarks/RecursiveRuleBenchmark.java
@@ -0,0 +1,83 @@
+/*
+ * SonarSource Language Recognizer
+ * Copyright (C) 2010 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.sslr.benchmarks;
+
+import com.google.common.base.Strings;
+import com.sonar.sslr.api.Grammar;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.GenerateMicroBenchmark;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.sonar.sslr.grammar.GrammarRuleKey;
+import org.sonar.sslr.grammar.LexerlessGrammarBuilder;
+import org.sonar.sslr.parser.ParseRunner;
+import org.sonar.sslr.parser.ParsingResult;
+
+@State
+@BenchmarkMode(Mode.Throughput)
+public class RecursiveRuleBenchmark {
+
+  private static enum MyGrammar implements GrammarRuleKey {
+    ROOT, RULE, TERM;
+
+    public static Grammar recursive() {
+      LexerlessGrammarBuilder b = common();
+      b.rule(RULE).is(TERM, b.optional("+", RULE));
+      return b.build();
+    }
+
+    public static Grammar greedy() {
+      LexerlessGrammarBuilder b = common();
+      b.rule(RULE).is(TERM, b.zeroOrMore("+", TERM));
+      return b.build();
+    }
+
+    private static LexerlessGrammarBuilder common() {
+      LexerlessGrammarBuilder b = LexerlessGrammarBuilder.create();
+      b.setRootRule(ROOT);
+      b.rule(ROOT).is(RULE, b.endOfInput());
+      b.rule(TERM).is("t");
+      return b;
+    }
+  }
+
+  private char[] input;
+  private ParseRunner recursive, greedy;
+
+  @Setup
+  public void setup() {
+    int n = Integer.getInteger("n", 3);
+    input = ("t" + Strings.repeat("+t", n)).toCharArray();
+    recursive = new ParseRunner(MyGrammar.recursive().getRootRule());
+    greedy = new ParseRunner(MyGrammar.greedy().getRootRule());
+  }
+
+  @GenerateMicroBenchmark
+  public ParsingResult recursive() {
+    return recursive.parse(input);
+  }
+
+  @GenerateMicroBenchmark
+  public ParsingResult greedy() {
+    return greedy.parse(input);
+  }
+
+}
