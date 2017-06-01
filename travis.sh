5e4a281abcecc9714633b0fed1d5eb639bd19e0b
Evgeny Mandrikov <mandrikov@gmail.com>
2017-06-01 18:51:37 +0200

    Update travis-utils to v36

--- travis.sh
+++ travis.sh
@@ -4,10 +4,9 @@ set -euo pipefail
 
 function installTravisTools {
   mkdir ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v28 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v36 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
-
 installTravisTools
 
 regular_mvn_build_deploy_analyze
