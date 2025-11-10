796dc7a8b48ebb346ea70456a7f2c479a0579a3d
Massimo Paladin <massimo.paladin@gmail.com>
2025-11-10 13:25:16 +0100

    SSLR-401 Update license header from SonarSource SA to SonarSource Sàrl

--- sslr-core/src/main/java/com/sonar/sslr/impl/channel/RegexpChannel.java
+++ sslr-core/src/main/java/com/sonar/sslr/impl/channel/RegexpChannel.java
@@ -1,6 +1,6 @@
 /*
  * SonarSource Language Recognizer
- * Copyright (C) 2010-2025 SonarSource SA
+ * Copyright (C) 2010-2025 SonarSource Sàrl
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
